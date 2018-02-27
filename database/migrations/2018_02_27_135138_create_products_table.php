1edc15e0ec8603575f39b29c909b17f47c80162d
Carlos Ferreira <carlos@especializati.com.br>
2018-02-27 10:57:19 -0300

    Define estrutura da tabela de produtos

--- database/migrations/2018_02_27_135138_create_products_table.php
+++ database/migrations/2018_02_27_135138_create_products_table.php
@@ -15,6 +15,8 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name', 100)->unique();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
