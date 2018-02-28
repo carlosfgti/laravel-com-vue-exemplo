3cd29f0851ce95fd730293a8182743516e290d31
Carlos Ferreira <conta@especializati.com.br>
2018-02-28 09:33:57 -0300

    Cria o método getResults() para listar os produtos, com fltros

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,42 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name', 'description', 'image'];
+
+
+
+    /**
+     * Filter Products
+     *
+     * @param  Array $data filtros
+     * @param  int $totalPage itens por página
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getResults(Array $data, int $totalPage)
+    {
+        // Se não existir filtro (name, description ou filter) retorna todos os resultados, paginados
+    	if (!isset($data['name']) && !isset($data['description']) && !isset($data['filter']))
+    		return $this->paginate($totalPage);
+
+        // Traz os resultados filtrados
+    	return $this->where(function ($query) use ($data) {
+    				if (isset($data['name'])) {
+    					$name = $data['name'];
+    					$query->where('name', 'LIKE', "%{$name}%");
+                    }
+                    
+                    if (isset($data['description'])) {
+    					$description = $data['description'];
+    					$query->where('description', 'LIKE', "%{$description}%");
+    				}
+
+    				if (isset($data['filter'])) {
+                        $filter = $data['filter'];
+
+                        $query->where('name', 'LIKE', "%{$filter}%")
+                                ->orWhere('description', 'LIKE', "%{$filter}%");
+                    }
+
+    			})->paginate($totalPage);// ->toSql();
+    }
 }
