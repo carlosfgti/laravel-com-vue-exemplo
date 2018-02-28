343763866d1daf51931d1a461a2d0cf41b0a3bf2
Carlos Ferreira <conta@especializati.com.br>
2018-02-28 11:21:28 -0300

    Adiciona exceções JWT

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -54,6 +54,11 @@ class Handler extends ExceptionHandler
         if( $exception instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException )
             return response()->json(['error' => 'method_not_allowed'], 405);
 
+        if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException)
+            return response()->json(['token_expired'], $e->getStatusCode());
+        else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException)
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
         return parent::render($request, $exception);
     }
 }
