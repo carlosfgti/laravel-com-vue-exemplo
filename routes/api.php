c1e693dd1cccf7e49a53bd4ed1685a475c747c36
Carlos Ferreira <conta@especializati.com.br>
2018-02-28 10:01:00 -0300

    Rota API Resource

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,4 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Rotas API
+$this->apiResource('products', 'Api\v1\ProductController');
