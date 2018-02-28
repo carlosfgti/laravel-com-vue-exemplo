e245f4cb64022776ff89e8dcd8ecb4a17c18f4ff
Carlos Ferreira <conta@especializati.com.br>
2018-02-28 11:09:20 -0300

    Controller de Login Api (token)

---
+++ app/Http/Controllers/Auth/ApiLoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ApiLoginController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        // grab credentials from the request
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // all good so return the token
+        return response()->json(compact('token'));
+    }
+}
