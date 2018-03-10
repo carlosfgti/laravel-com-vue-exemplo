9e19a45347124cb11752cbf2651e5c5e8397d33c
Carlos Ferreira <conta@especializati.com.br>
2018-03-10 09:09:07 -0300

    Adicionar Rota para Atualização de TOKEN JWT

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ $this->group(['prefix' => 'v1'], function() {
     // Rota de autenticação
     $this->post('auth', 'Auth\ApiLoginController@authenticate');
 
+    // Atualização do TOKEN
+    $this->post('auth-refresh', 'Auth\ApiLoginController@refreshToken');
+
     // Rotas autenticadas por token JWT
     $this->group(['middleware' => 'jwt.auth'], function () {
         // Retorna o usuário autenticado pelo token
